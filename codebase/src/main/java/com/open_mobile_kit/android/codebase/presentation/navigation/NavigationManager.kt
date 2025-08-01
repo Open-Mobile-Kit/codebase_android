2f9525d9347421802a5b63589e362f5e672e6dd9
alvin <phuong.vu.dev@gmail.com>
2025-08-01 15:19:08 +0700

    Refactor NavigationManager to support dynamic navigation guards and observers

--- codebase/src/main/java/com/open_mobile_kit/android/codebase/presentation/navigation/NavigationManager.kt
+++ codebase/src/main/java/com/open_mobile_kit/android/codebase/presentation/navigation/NavigationManager.kt
@@ -9,34 +9,70 @@ import javax.inject.Singleton
 @Singleton
 class NavigationManager @Inject constructor(
 
-) {
+) : AppNavigation {
     private val observers: MutableSet<@JvmSuppressWildcards NavigationObserver> = mutableSetOf()
+    private val guards: MutableSet<@JvmSuppressWildcards NavigationGuard> = mutableSetOf()
     private val _commands = MutableSharedFlow<NavigationCommand>()
     val commands = _commands.asSharedFlow()
+
+    fun addObserver(observer: NavigationObserver) {
+        observers.add(observer)
+    }
+
+    fun removeObserver(observer: NavigationObserver) {
+        observers.remove(observer)
+    }
+
+    fun addGuard(guard: NavigationGuard) {
+        guards.add(guard)
+    }
+
+    fun removeGuard(guard: NavigationGuard) {
+        guards.remove(guard)
+    }
+
 
     suspend fun navigate(command: NavigationCommand) {
         _commands.emit(command)
         observers.forEach { it.onNavigate(command) }
     }
 
-    suspend fun  pop() {
+
+    suspend fun validate(guards: List<NavigationGuard>): Boolean {
+        return guards.all { it.canNavigate() == null } && this.guards.all { it.canNavigate() == null }
+    }
+
+    override suspend fun go(
+        destination: String,
+        args: Bundle?,
+        guards: List<NavigationGuard>
+    ) {
+        if (validate(guards)) {
+            navigate(NavigationCommand.Go(destination, args))
+        }
+    }
+
+    override suspend fun push(
+        destination: String,
+        args: Bundle?,
+        guards: List<NavigationGuard>
+    ) {
+        if (validate(guards)) {
+            navigate(NavigationCommand.Push(destination, args))
+        }
+    }
+
+    override suspend fun replace(
+        destination: String,
+        args: Bundle?,
+        guards: List<NavigationGuard>
+    ) {
+        if (validate(guards)) {
+            navigate(NavigationCommand.Replace(destination, args))
+        }
+    }
+
+    override suspend fun pop() {
         navigate(NavigationCommand.Pop)
-    }
-
-    suspend fun push(destination: String, args: Bundle? = null) {
-        navigate(NavigationCommand.Push(destination, args))
-    }
-
-
-    suspend fun go(destination: String, args: Bundle? = null) {
-        navigate(NavigationCommand.Go(destination, args))
-    }
-
-    suspend fun replace(destination: String, args: Bundle? = null) {
-        navigate(NavigationCommand.Replace(destination, args))
-    }
-
-    fun addObserver(observer: NavigationObserver) {
-        observers.add(observer)
     }
 }
